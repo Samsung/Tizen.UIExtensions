7128c4907900ceacca9c8d2ab1c9921dd2ef98cd
JoonghyunCho <jh5.cho@samsung.com>
2021-04-12 10:21:00 +0900

    Add Shapes: PolygonView PolylineView

--- src/Tizen.UIExtensions.ElmSharp/Extensions/GeometryExtensions.cs
+++ src/Tizen.UIExtensions.ElmSharp/Extensions/GeometryExtensions.cs
@@ -1,24 +1,24 @@
 ﻿using ElmSharp;
-using Point = Tizen.UIExtensions.Common.Point;
-using Rect = Tizen.UIExtensions.Common.Rect;
-using Size = Tizen.UIExtensions.Common.Size;
+using CPoint = Tizen.UIExtensions.Common.Point;
+using CRect = Tizen.UIExtensions.Common.Rect;
+using CSize = Tizen.UIExtensions.Common.Size;
 using ERect = ElmSharp.Rect;
 
 namespace Tizen.UIExtensions.ElmSharp
 {
     public static class GeometryExtensions
     {
-        public static void UpdateSize(this EvasObject view, Size size)
+        public static void UpdateSize(this EvasObject view, CSize size)
         {
             view.Resize((int)size.Width, (int)size.Height);
         }
 
-        public static void UpdatePosition(this EvasObject view, Point position)
+        public static void UpdatePosition(this EvasObject view, CPoint position)
         {
             view.Move((int)position.X, (int)position.Y);
         }
 
-        public static void UpdateBounds(this EvasObject view, Rect bounds)
+        public static void UpdateBounds(this EvasObject view, CRect bounds)
         {
             view.Geometry = new ERect((int)bounds.X, (int)bounds.Y, (int)bounds.Width, (int)bounds.Height);
         }
