3207826b0433a92ac1b0dafe41fe237a8154ed3c
Jay Cho <chojoong@gmail.com>
2021-04-13 10:43:00 +0900

    Add Shapes: PathView

---
+++ src/Tizen.UIExtensions.ElmSharp/Shapes/PathView.cs
@@ -0,0 +1,22 @@
+﻿using ElmSharp;
+using SkiaSharp;
+
+namespace Tizen.UIExtensions.ElmSharp
+{
+    public class PathView : ShapeView
+    {
+        public PathView(EvasObject parent) : base(parent)
+        {
+        }
+
+        public void UpdateData(SKPath path)
+        {
+            UpdateShape(path);
+        }
+
+        public void UpdateTransform(SKMatrix transform)
+        {
+            UpdateShapeTransform(transform);
+        }
+    }
+}
