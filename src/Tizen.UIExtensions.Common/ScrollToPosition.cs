808be4b572c8c25a41466c936b21938a382f4ac8
Seungkeun Lee <sngn.lee@samsung.com>
2021-04-22 16:11:23 +0900

    Implement Item Selection and ScrollTo

---
+++ src/Tizen.UIExtensions.Common/ScrollToPosition.cs
@@ -0,0 +1,28 @@
+﻿namespace Tizen.UIExtensions.Common
+{
+    /// <summary>
+    /// Enumerates values that describe a scroll request.
+    /// </summary>
+    public enum ScrollToPosition
+    {
+        /// <summary>
+        /// Scroll to make a specified list item visible.
+        /// </summary>
+        MakeVisible = 0,
+
+        /// <summary>
+        /// Scroll to the start of a list.
+        /// </summary>
+        Start = 1,
+
+        /// <summary>
+        /// Scroll to the center of a list.
+        /// </summary>
+        Center = 2,
+
+        /// <summary>
+        /// Scroll to the end of a list.
+        /// </summary>
+        End = 3
+    }
+}
