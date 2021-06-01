ccd19f2b3d4d3f7185123a216e54570864ad05f5
Seungkeun Lee <sngn.lee@samsung.com>
2021-06-01 17:29:26 +0900

    [NUI] Add SKCanvasView / SKGLSurfaceView

---
+++ src/Tizen.UIExtensions.NUI/CustomRenderingView.cs
@@ -0,0 +1,55 @@
+﻿using SkiaSharp.Views.Tizen;
+using System;
+using System.Threading;
+using Tizen.NUI;
+using NImageView = Tizen.NUI.BaseComponents.ImageView;
+
+namespace Tizen.UIExtensions.NUI
+{
+    public abstract class CustomRenderingView : NImageView
+    {
+        bool _redrawRequest;
+        PropertyNotification _resized;
+
+        protected SynchronizationContext MainloopContext { get; }
+
+        protected CustomRenderingView()
+        {
+            MainloopContext = SynchronizationContext.Current ?? throw new InvalidOperationException("Must create on main thread");
+            _resized = AddPropertyNotification("Size", PropertyCondition.Step(0.1f));
+            _resized.Notified += OnResized;
+        }
+
+        public event EventHandler<SKPaintSurfaceEventArgs>? PaintSurface;
+
+        public void Invalidate()
+        {
+            if (!_redrawRequest)
+            {
+                _redrawRequest = true;
+                MainloopContext.Post((s) =>
+                {
+                    _redrawRequest = false;
+                    if (!Disposed)
+                    {
+                        OnDrawFrame();
+                    }
+                }, null);
+            }
+        }
+
+        protected abstract void OnResized();
+
+        protected abstract void OnDrawFrame();
+
+        protected void SendPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            PaintSurface?.Invoke(this, e);
+        }
+
+        void OnResized(object source, PropertyNotification.NotifyEventArgs e)
+        {
+            OnResized();
+        }
+    }
+}
