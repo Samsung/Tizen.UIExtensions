d60fc50132bc897f275da03ca4450caf93bf5e1e
Sunghyun Min <shyun.min@samsung.com>
2021-07-22 19:41:02 +0900

    [Elmsharp] Add Tabs

---
+++ src/Tizen.UIExtensions.ElmSharp/Tabs.cs
@@ -0,0 +1,33 @@
+﻿using ElmSharp;
+
+namespace Tizen.UIExtensions.ElmSharp
+{
+    public class Tabs : Toolbar, ITabs
+    {
+        TabsScrollType _type;
+
+        public Tabs(EvasObject parent) :base(parent)
+        {
+            Style = ThemeConstants.Toolbar.Styles.Material;
+            SelectionMode = ToolbarSelectionMode.Always;
+        }
+
+        public TabsScrollType ScrollType
+        {
+            get => _type;
+            set
+            {
+                switch (value)
+                {
+                    case TabsScrollType.Fixed:
+                        this.ShrinkMode = ToolbarShrinkMode.Expand;
+                        break;
+                    case TabsScrollType.Scrollable:
+                        this.ShrinkMode = ToolbarShrinkMode.Scroll;
+                        break;
+                }
+                _type = value;
+            }
+        }
+    }
+}
