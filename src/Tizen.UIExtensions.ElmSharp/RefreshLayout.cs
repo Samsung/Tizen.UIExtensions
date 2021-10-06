be9e8a898bf8dfb2fce8d4c497b065d721db0150
Jay Cho <jh5.cho@samsung.com>
2021-10-06 12:26:20 +0900

    [ElmSharp] Add RefreshLayout

---
+++ src/Tizen.UIExtensions.ElmSharp/RefreshLayout.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using ElmSharp;
+using Tizen.UIExtensions.Common.Internal;
+
+namespace Tizen.UIExtensions.ElmSharp
+{
+    enum RefreshState
+    {
+        Idle,
+        Drag,
+        Loading,
+    }
+
+    /// <summary>
+    /// RefreshLayout is a container that holds a single content and allows you to pull down to refresh content with showing refresh animation.
+    /// </summary>
+    public class RefreshLayout : Box, IAnimatable
+    {
+        RefreshIcon _refreshIcon;
+        Box _contentLayout;
+        EvasObject? _content;
+        GestureLayer _gestureLayer;
+        RefreshState _refreshState;
+        bool _shouldRefresh;
+        bool _isRefreshing;
+        bool _isRefreshEnabled;
+        int _initialIconGeometryY;
+
+        int _maximumDistance = ThemeConstants.RefreshLayout.Resources.RefreshDistance;
+        int _minimumSize = ThemeConstants.RefreshLayout.Resources.MinimumLayoutSize;
+        float _iconSize = ThemeConstants.RefreshLayout.Resources.IconSize;
+        uint _animationLength = ThemeConstants.RefreshLayout.Resources.RefreshAnimationLength;
+
+        /// <summary>
+        /// Initializes a new instance of the RefreshLayout class.
+        /// </summary>
+        /// <param name="parent"></param>
+        public RefreshLayout(EvasObject parent) : base(parent)
+        {
+            MinimumHeight = _minimumSize;
+            _refreshState = RefreshState.Idle;
+            _refreshIcon = new RefreshIcon(parent);
+            _contentLayout = new Box(parent);
+            _refreshIcon.Show();
+            _contentLayout.Show();
+
+            _gestureLayer = new GestureLayer(parent);
+            _gestureLayer.Attach(_contentLayout);
+            _gestureLayer.SetMomentumCallback(GestureLayer.GestureState.Move, OnMoved);
+            _gestureLayer.SetMomentumCallback(GestureLayer.GestureState.End, OnEnded);
+            _gestureLayer.SetMomentumCallback(GestureLayer.GestureState.Abort, OnEnded);
+
+            this.PackEnd(_refreshIcon);
+            this.PackEnd(_contentLayout);
+
+            SetLayoutCallback(OnLayoutUpdate);
+        }
+
+        /// <summary>
+        /// Invokes when refreshing starts by pulling down the content.
+        /// </summary>
+        public event EventHandler? Refreshing;
+
+        public bool IsRefreshEnabled
+        {
+            get
+            {
+                return _isRefreshEnabled;
+            }
+            set
+            {
+                _isRefreshEnabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the refreshing status of RefreshLayout.
+        /// Refresh animation starts when it is set to true.
+        /// </summary>
+        public bool IsRefreshing
+        {
+            get
+            {
+                return _isRefreshing;
+            }
+            set
+            {
+                if (_isRefreshing != value)
+                {
+                    _isRefreshing = value;
+                    if (_isRefreshing)
+                    {
+                        BeginRefreshing(false);
+                    }
+                    else
+                    {
+                        ResetRefreshing();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the color of the refresh icon.
+        /// </summary>
+        public Common.Color RefreshIconColor
+        {
+            get
+            {
+                return _refreshIcon.Color;
+            }
+            set
+            {
+                _refreshIcon.Color = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the content of the RefreshLayout.
+        /// </summary>
+        public EvasObject? Content
+        {
+            get
+            {
+                return _content;
+            }
+            set
+            {
+                if (_content != null)
+                {
+                    _contentLayout.UnPackAll();
+                }
+                _content = value;
+                _contentLayout.PackEnd(_content);
+            }
+        }
+
+        void OnLayoutUpdate()
+        {
+            var iconGeometryX = Geometry.X + (Geometry.Width - _refreshIcon.Geometry.Width) / 2;
+            var iconBottomPadding = (int)_iconSize / 2;
+            _initialIconGeometryY = Geometry.Y - (_refreshIcon.Geometry.Height+ iconBottomPadding);
+            var iconHeight = _refreshIcon.Geometry.Height + iconBottomPadding;
+            _refreshIcon.Geometry = new Rect(iconGeometryX, _initialIconGeometryY, _refreshIcon.Geometry.Width, iconHeight);
+            _contentLayout.Geometry = Geometry;
+            if (_content != null)
+            {
+                _content.Geometry = Geometry;
+            }
+        }
+
+        void OnMoved(GestureLayer.MomentumData moment)
+        {
+            if (_refreshState == RefreshState.Idle && _isRefreshEnabled)
+            {
+                _refreshState = RefreshState.Drag;
+            }
+
+            if (_refreshState == RefreshState.Drag)
+            {
+                var dy = moment.Y2 - moment.Y1;
+                if (dy < 0)
+                    return;
+                MoveLayout(dy);
+            }
+        }
+
+        void OnEnded(GestureLayer.MomentumData moment)
+        {
+            if (_refreshState != RefreshState.Drag)
+            {
+                return;
+            }
+
+            if (_shouldRefresh)
+            {
+                BeginRefreshing(true);
+                _shouldRefresh = false;
+            }
+            else
+            {
+                ResetRefreshing();
+            }
+        }
+
+        void BeginRefreshing(bool isPulledRefresh)
+        {
+            var movedDistance = GetMovedDistance();
+            var refreshDistance = _refreshIcon.Geometry.Height + _maximumDistance;
+            var contentDistanceDiff = refreshDistance - movedDistance;
+            var _refreshStartAnimation = new Animation(v => _contentLayout.Move(Geometry.X, Geometry.Y + movedDistance + (int)v), 0, contentDistanceDiff, Easing.Linear);
+            _refreshStartAnimation.Commit(this, "RefreshBegin", length: _animationLength);
+
+            if (!isPulledRefresh)
+            {
+                var currentIconGeometryY = Geometry.Y + _refreshIcon.Geometry.Y;
+                var iconDistanceDiff = _maximumDistance - currentIconGeometryY;
+                var _refreshIconBeginAnimation = new Animation(v => _refreshIcon.Move(_refreshIcon.Geometry.X, currentIconGeometryY + (int)v), 0, iconDistanceDiff, Easing.Linear);
+                _refreshIconBeginAnimation.Commit(this, "RefreshIconBegin", length: _animationLength);
+            }
+
+            _refreshState = RefreshState.Loading;
+            _isRefreshing = true;
+            _refreshIcon.IsRunning = true;
+            Refreshing?.Invoke(this, EventArgs.Empty);
+        }
+
+        void ResetRefreshing()
+        {
+            var movedDistance = GetMovedDistance();
+            var _refreshResetAnimation = new Animation(v => _contentLayout.Move(Geometry.X, Geometry.Y + movedDistance - (int)v), 0, movedDistance, Easing.Linear);
+            _refreshResetAnimation.Commit(this, "RefreshReset", length: _animationLength);
+
+            var currentIconGeometryY = _refreshIcon.Geometry.Y;
+            var _refreshIconResetAnimation = new Animation(v => _refreshIcon.Move(_refreshIcon.Geometry.X, currentIconGeometryY - (int)v), 0, movedDistance, Easing.Linear);
+            _refreshIconResetAnimation.Commit(this, "RefreshIconReset", length: _animationLength);
+
+            _refreshState = RefreshState.Idle;
+            _refreshIcon.IsRunning = false;
+        }
+
+        int GetMovedDistance()
+        {
+            return _contentLayout.Geometry.Y - Geometry.Y;
+        }
+
+        void MoveLayout(int distance)
+        {
+            var iconDistance = _initialIconGeometryY + distance;
+            if (iconDistance > _maximumDistance)
+            {
+                iconDistance = _maximumDistance;
+                _shouldRefresh = true;
+            }
+            else
+            {
+                _shouldRefresh = false;
+            }
+            _refreshIcon.Move(_refreshIcon.Geometry.X, iconDistance);
+            _contentLayout.Move(_contentLayout.Geometry.X, Geometry.Y + distance);
+        }
+
+        void IAnimatable.BatchBegin() {}
+
+        void IAnimatable.BatchCommit() {}
+    }
+}
---
+++ test/ElmSharpExGallery/TC/RefreshLayoutTest1.cs
@@ -0,0 +1,103 @@
+using System.Threading.Tasks;
+using Tizen.UIExtensions.ElmSharp;
+using Tizen.UIExtensions.Common;
+using Color = Tizen.UIExtensions.Common.Color;
+
+namespace ElmSharpExGallery.TC
+{
+    public class RefreshLayoutTest1 : TestCaseBase
+    {
+        public override string TestName => "RefreshLayoutTest1";
+
+        public override string TestDescription => "Test RefreshLayout";
+
+        public override void Run(ElmSharp.Box parent)
+        {
+            var refreshLayout = new RefreshLayout(parent)
+            {
+                WeightX = 1,
+                WeightY = 1,
+                AlignmentY = -1,
+                AlignmentX = -1,
+            };
+            var box = new Tizen.UIExtensions.ElmSharp.Box(parent)
+            {
+                WeightX = 1,
+                WeightY = 1,
+                AlignmentY = -1,
+                AlignmentX = -1,
+                BackgroundColor = ElmSharp.Color.Gray
+            };
+            var changeIconButton = new Tizen.UIExtensions.ElmSharp.Button(parent)
+            {
+                Text = "Change Icon Color",
+                WeightX = 1,
+                WeightY = 1,
+                AlignmentX = -1,
+                AlignmentY = -1,
+            };
+            changeIconButton.Move(100, 300);
+            changeIconButton.Resize(400, 100);
+            changeIconButton.Clicked += (s, e) =>
+            {
+                if (refreshLayout.RefreshIconColor == Color.Default)
+                    refreshLayout.RefreshIconColor = Color.Red;
+                else
+                    refreshLayout.RefreshIconColor = Color.Default;
+            };
+            changeIconButton.Show();
+            var statusLabel = new Tizen.UIExtensions.ElmSharp.Label(parent)
+            {
+                Text = "Idle",
+                WeightX = 1,
+                WeightY = 1,
+                AlignmentX = -1,
+                AlignmentY = -1,
+                TextColor = Color.Beige,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 30
+            };
+            statusLabel.Move(100, 200);
+            statusLabel.Resize(300, 100);
+            statusLabel.Show();
+
+            var changeRefreshingButton = new Tizen.UIExtensions.ElmSharp.Button(parent)
+            {
+                Text = $"Start Refreshing",
+                WeightX = 1,
+                WeightY = 1,
+                AlignmentX = -1,
+                AlignmentY = -1,
+            };
+            changeRefreshingButton.Move(100, 400);
+            changeRefreshingButton.Resize(400, 100);
+            changeRefreshingButton.Clicked += (s, e) =>
+            {
+                refreshLayout.IsRefreshing = true;
+            };
+            changeRefreshingButton.Show();
+
+            box.PackEnd(statusLabel);
+            box.PackEnd(changeIconButton);
+            box.PackEnd(changeRefreshingButton);
+            box.Show();
+
+            refreshLayout.Content = box;
+            refreshLayout.IsRefreshEnabled = true;
+            refreshLayout.Refreshing += async (s, e) =>
+            {
+                statusLabel.Text = "Refreshing";
+                await Task.Delay(2000);
+                refreshLayout.IsRefreshing = false;
+                statusLabel.Text = "Refreshed";
+
+            };
+
+            refreshLayout.Resize(300, 900);
+            refreshLayout.Show();
+            parent.PackEnd(refreshLayout);
+        }
+    }
+}
