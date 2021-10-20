00a4dd40e7ccf30f1dabdca8b7f1a9b22720497d
Seungkeun Lee <sngn.lee@samsung.com>
2021-10-20 15:29:27 +0900

    Fix GraphicsView Editor/Entry

--- src/Tizen.UIExtensions.NUI/GraphicsView/Entry.cs
+++ src/Tizen.UIExtensions.NUI/GraphicsView/Entry.cs
@@ -8,14 +8,16 @@ namespace Tizen.UIExtensions.NUI.GraphicsView
     /// <summary>
     /// A control that can edit a single line of text.
     /// </summary>
-    public class Entry : GraphicsView<EntryDrawable>, IEntry
+    public class Entry : View, IEntry, IMeasurable
     {
+        GraphcisEntry _entry;
+
         /// <summary>
         /// Initializes a new instance of the Entry class.
         /// </summary>
         public Entry()
         {
-            Layout = new Tizen.NUI.LinearLayout();
+            Layout = new Tizen.NUI.AbsoluteLayout();
             EmbedEntry = new TEntry
             {
                 WidthSpecification = LayoutParamPolicies.MatchParent,
@@ -23,12 +25,22 @@ namespace Tizen.UIExtensions.NUI.GraphicsView
                 VerticalTextAlignment = TextAlignment.Center,
                 Margin = new Tizen.NUI.Extents((ushort)(12 * DeviceInfo.ScalingFactor), (ushort)(40 * DeviceInfo.ScalingFactor), (ushort)(12 * DeviceInfo.ScalingFactor), 0)
             };
-            EmbedEntry.FocusGained += OnFocused;
-            EmbedEntry.FocusLost += OnUnfocused;
-            EmbedEntry.TextChanged += OnTextChanged;
-            Add(EmbedEntry);
+            _entry = new GraphcisEntry(EmbedEntry)
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+            };
+            Add(_entry);
 
-            Drawable = new EntryDrawable(this);
+            // NUI AbsoluteLayout not support Margin, so add a internal view as LinearLayout
+            var marginView = new View
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                Layout = new Tizen.NUI.LinearLayout(),
+            };
+            Add(marginView);
+            marginView.Add(EmbedEntry);
         }
 
         public TEntry EmbedEntry { get; }
@@ -36,10 +48,10 @@ namespace Tizen.UIExtensions.NUI.GraphicsView
         /// <summary>
         /// Gets or sets the text of the entry
         /// </summary>
-        public string Text
+        public string Text 
         {
-            get => EmbedEntry.Text;
-            set => EmbedEntry.Text = value;
+            get => _entry.Text;
+            set => _entry.Text = value;
         }
 
         /// <summary>
@@ -47,8 +59,8 @@ namespace Tizen.UIExtensions.NUI.GraphicsView
         /// </summary>
         public Color TextColor
         {
-            get => EmbedEntry.TextColor;
-            set => EmbedEntry.TextColor = value;
+            get => _entry.TextColor;
+            set => _entry.TextColor = value;
         }
 
         /// <summary>
@@ -56,8 +68,8 @@ namespace Tizen.UIExtensions.NUI.GraphicsView
         /// </summary>
         public string Placeholder
         {
-            get => GetProperty<string>(nameof(Placeholder));
-            set => SetProperty(nameof(Placeholder), value);
+            get => _entry.Placeholder;
+            set => _entry.Placeholder = value;
         }
 
         /// <summary>
@@ -65,25 +77,80 @@ namespace Tizen.UIExtensions.NUI.GraphicsView
         /// </summary>
         public Color PlaceholderColor
         {
-            get => GetProperty<Color>(nameof(PlaceholderColor));
-            set => SetProperty(nameof(PlaceholderColor), value);
+            get => _entry.PlaceholderColor;
+            set => _entry.PlaceholderColor = value;
         }
+
+        public bool IsFocused => _entry.IsFocused;
 
         /// <summary>
         /// Gets or sets the color which will fill the background
         /// </summary>
         public new Color BackgroundColor
         {
-            get => GetProperty<Color>(nameof(BackgroundColor));
-            set => SetProperty(nameof(BackgroundColor), value);
+            get => _entry.BackgroundColor;
+            set => _entry.BackgroundColor = value;
         }
-        Color IEntry.BackgroundColor => BackgroundColor;
+        Color IEntry.BackgroundColor => _entry.BackgroundColor;
 
-        public bool IsFocused => EmbedEntry.HasFocus();
-
-        void OnTextChanged(object? sender, TextField.TextChangedEventArgs e)
+        public Size Measure(double availableWidth, double availableHeight)
         {
-            Invalidate();
+            return _entry.Measure(availableWidth, availableHeight);
+        }
+
+        class GraphcisEntry : GraphicsView<EntryDrawable>, IEntry
+        {
+            public GraphcisEntry(TEntry entry)
+            {
+                EmbedEntry = entry;
+                EmbedEntry.FocusGained += OnFocused;
+                EmbedEntry.FocusLost += OnUnfocused;
+                EmbedEntry.TextChanged += OnTextChanged;
+
+                Drawable = new EntryDrawable(this);
+            }
+
+            public TEntry EmbedEntry { get; }
+
+            public string Text
+            {
+                get => EmbedEntry.Text;
+                set => EmbedEntry.Text = value;
+            }
+
+            public Color TextColor
+            {
+                get => EmbedEntry.TextColor;
+                set => EmbedEntry.TextColor = value;
+            }
+
+            public string Placeholder
+            {
+                get => GetProperty<string>(nameof(Placeholder));
+                set => SetProperty(nameof(Placeholder), value);
+            }
+
+
+            public Color PlaceholderColor
+            {
+                get => GetProperty<Color>(nameof(PlaceholderColor));
+                set => SetProperty(nameof(PlaceholderColor), value);
+            }
+
+            public new Color BackgroundColor
+            {
+                get => GetProperty<Color>(nameof(BackgroundColor));
+                set => SetProperty(nameof(BackgroundColor), value);
+            }
+            Color IEntry.BackgroundColor => BackgroundColor;
+
+            public bool IsFocused => EmbedEntry.HasFocus();
+
+            void OnTextChanged(object? sender, TextField.TextChangedEventArgs e)
+            {
+                Invalidate();
+            }
         }
     }
+
 }
