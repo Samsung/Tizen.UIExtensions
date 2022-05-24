3409a169f159db0b8a6b5f65f1b3100589e6cd3f
Seungkeun Lee <sngn.lee@samsung.com>
2022-05-24 15:27:47 +0900

    Update RefreshIcon

--- src/Tizen.UIExtensions.NUI/GraphicsView/RefreshIcon.cs
+++ src/Tizen.UIExtensions.NUI/GraphicsView/RefreshIcon.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using ElmSharp;
+using Tizen.UIExtensions.Common;
 using Tizen.UIExtensions.Common.GraphicsView;
 
-namespace Tizen.UIExtensions.ElmSharp.GraphicsView
+namespace Tizen.UIExtensions.NUI.GraphicsView
 {
     /// <summary>
     /// A visual control used to indicate that refreshing is ongoing.
@@ -12,22 +12,21 @@ namespace Tizen.UIExtensions.ElmSharp.GraphicsView
         /// <summary>
         /// Initializes a new instance of the RefreshIcon.
         /// </summary>
-        public RefreshIcon(EvasObject parent) : base(parent)
+        public RefreshIcon()
         {
             Drawable = new RefreshIconDrawable(this);
-
-            var size = Drawable.Measure(double.PositiveInfinity, double.PositiveInfinity);
-            Resize((int)size.Width, (int)size.Height);
         }
 
         /// <summary>
         /// Gets or sets the background of RefreshIcon.
         /// </summary>
-        public new Common.Color BackgroundColor
+        public new Color BackgroundColor
         {
-            get => GetProperty<Common.Color>(nameof(BackgroundColor));
+            get => GetProperty<Color>(nameof(BackgroundColor));
             set => SetProperty(nameof(BackgroundColor), value);
         }
+
+        Color IRefreshIcon.BackgroundColor => BackgroundColor;
 
         /// <summary>
         /// Gets or sets the value indicating if the RefreshIcon is running.
@@ -38,6 +37,8 @@ namespace Tizen.UIExtensions.ElmSharp.GraphicsView
             set => SetProperty(nameof(IsRunning), value);
         }
 
+        bool IRefreshIcon.IsRunning => IsRunning;
+
         /// <summary>
         /// Gets or sets the value indicating if the RefreshIcon is pulling.
         /// </summary>
@@ -47,14 +48,17 @@ namespace Tizen.UIExtensions.ElmSharp.GraphicsView
             set => SetProperty(nameof(IsPulling), value);
         }
 
+        bool IRefreshIcon.IsPulling => IsPulling;
+
         /// <summary>
         /// Gets or sets the Color of the RefreshIcon.
         /// </summary>
-        public new Common.Color Color
+        public new Color Color
         {
-            get => GetProperty<Common.Color>(nameof(Color));
+            get => GetProperty<Color>(nameof(Color));
             set => SetProperty(nameof(Color), value);
         }
+        Color IRefreshIcon.Color => Color;
 
         /// <summary>
         /// Gets or sets the value(0.0 ~ 1.0) indicating the icon pulling rate.
@@ -64,5 +68,17 @@ namespace Tizen.UIExtensions.ElmSharp.GraphicsView
             get => GetProperty<float>(nameof(PullDistance));
             set => SetProperty(nameof(PullDistance), Math.Clamp(value, 0, 1));
         }
+
+        float IRefreshIcon.PullDistance => PullDistance;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Drawable?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
     }
 }
