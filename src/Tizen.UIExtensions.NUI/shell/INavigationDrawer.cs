4bacfaa7bdd2529d9acc521588cd956036efef8f
Sunghyun Min <shyun.min@samsung.com>
2022-07-15 19:20:06 +0900

    Add INavigationDrawer interface

---
+++ src/Tizen.UIExtensions.NUI/shell/INavigationDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tizen.NUI.BaseComponents;
+using Tizen.UIExtensions.Common;
+
+namespace Tizen.UIExtensions.NUI
+{
+    public interface INavigationDrawer
+    {
+        View? Drawer { get; set; }
+
+        View? Content { get; set; }
+
+        View? BackDrop { get; set; }
+
+        bool IsOpened { get; }
+
+        double DrawerWidth { get; set; }
+
+        bool IsGestureEnabled { get; set; }
+
+        DrawerBehavior DrawerBehavior { get; set; }
+
+        event EventHandler Toggled;
+    }
+}
